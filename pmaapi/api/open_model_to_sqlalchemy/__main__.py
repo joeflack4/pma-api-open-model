a9377414126458ce2c3ff9b089c6f9c41b4e2b9b
joeflack4 <joeflack4@gmail.com>
2017-08-02 17:15:59 -0400

    Completed basic scaffolding for OpenModelToSqlAlchemy.

--- pmaapi/api/open_model_to_sqlalchemy/__main__.py
+++ pmaapi/api/open_model_to_sqlalchemy/__main__.py
@@ -2,13 +2,51 @@
 # -*- coding: utf-8 -*-
 """Open Model to SqlAlchemy"""
 import yaml
+from copy import copy
+from pmaapi.config import MODEL_FILE
+
+
+class OpenModelToSqlAlhemy:
+    """Open Model to SqlAlchemy"""
+
+    def __init__(self, file=None):
+        """Initialize"""
+        self.yaml, self.dict, self.sqlalchemy = None, None, None
+        self.data = self.load(file, return_data=True) if file else None
+        self.source_data = copy(self.data)
+
+    def get_data(self, format):
+        """Get data in specified format."""
+        return self.yaml if format is 'yaml' \
+            else self.dict if format in ('dict', 'dictionary') \
+            else self.sqlalchemy if format is 'sqlalchemy' \
+            else self.yaml
+
+    def load(self, file, return_data=False):
+        """Loads file and sets data in multiple formats.
+
+        Side Effects:
+            self.yaml (yaml): Set.
+            self.dict (dict): Set.
+            self.sqlalchemy (SqlAlchemy.Model): Set.
+
+        Returns:
+            yaml: If return_data.
+        """
+        with open(file, 'r') as stream:
+            try:
+                self.yaml = yaml.load(stream)
+                self.dict = ''  # TODO
+                self.sqlalchemy = ''  # TODO
+            except yaml.YAMLError as exc:
+                print(exc)
+
+        if return_data:
+            return self.yaml
 
 if __name__ == '__main__':
-    import yaml
-
-    with open("example.yaml", 'r') as stream:
-        try:
-            print(yaml.load(stream))
-        except yaml.YAMLError as exc:
-            print(exc)
-
+    # Testing
+    model = OpenModelToSqlAlhemy()
+    model.load(MODEL_FILE)
+    print(model.yaml, model.dict, model.sqlalchemy)
+    # pass
